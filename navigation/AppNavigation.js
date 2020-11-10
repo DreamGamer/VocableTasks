c30676af097f7145138ac86a19c55a58a1967d37
Maurice Bertram <Stricker78@live.de>
2020-11-10 13:46:51 +0100

    Added EditVocableScreen

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import React from "react";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { createStackNavigator } from "react-navigation-stack";
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
 // Import Screens
@@ -9,67 +9,66 @@ import OptionsScreen from "../screens/options/OptionsScreen";
 import VocabularysScreen from "../screens/learning/VocabularysScreen";
 import LearnScreen from "../screens/learning/LearnScreen";
 import AddVocableScreen from "../screens/learning/AddVocableScreen";
-
-
+import EditVocableScreen from "../screens/learning/EditVocableScreen";
 
 const vocabularyStackNavigator = createStackNavigator({
-    vocabularys: {
-        screen: VocabularysScreen
-    },
-    addVocable: {
-        screen: AddVocableScreen
-    }
+  vocabularys: {
+    screen: VocabularysScreen,
+  },
+  addVocable: {
+    screen: AddVocableScreen,
+  },
+  editVocable: {
+    screen: EditVocableScreen,
+  },
 });
 
 const learnStackNavigator = createStackNavigator({
-    learn: {
-        screen: LearnScreen
-    }
+  learn: {
+    screen: LearnScreen,
+  },
 });
-
 
 const optionsStackNavigator = createStackNavigator({
-    options: {
-        screen: OptionsScreen
-    }
+  options: {
+    screen: OptionsScreen,
+  },
 });
-
 
 const learningBottomTabNavigator = createBottomTabNavigator({
-    vocabulary: {
-        screen: vocabularyStackNavigator,
-        navigationOptions: {
-            tabBarLabel: "Vocabulary",
-            tabBarIcon: (tabInformationn) => {
-                return <Ionicons name="md-book" size={25} color={tabInformationn.tintColor} />
-            }
-        }
+  vocabulary: {
+    screen: vocabularyStackNavigator,
+    navigationOptions: {
+      tabBarLabel: "Vocabulary",
+      tabBarIcon: (tabInformationn) => {
+        return <Ionicons name="md-book" size={25} color={tabInformationn.tintColor} />;
+      },
     },
-    learn: {
-        screen: learnStackNavigator,
-        navigationOptions: {
-            tabBarLabel: "Learn",
-            tabBarIcon: (tabInformationn) => {
-                return <Ionicons name="md-create" size={25} color={tabInformationn.tintColor} />
-            }
-        }
-    }
+  },
+  learn: {
+    screen: learnStackNavigator,
+    navigationOptions: {
+      tabBarLabel: "Learn",
+      tabBarIcon: (tabInformationn) => {
+        return <Ionicons name="md-create" size={25} color={tabInformationn.tintColor} />;
+      },
+    },
+  },
 });
 
-
 const DrawerNavigator = createDrawerNavigator({
-    learning: {
-        screen: learningBottomTabNavigator,
-        navigationOptions: {
-            drawerLabel: "Learning"
-        }
+  learning: {
+    screen: learningBottomTabNavigator,
+    navigationOptions: {
+      drawerLabel: "Learning",
     },
-    options: {
-        screen: optionsStackNavigator,
-        navigationOptions: {
-            drawerLabel: "Options"
-        }
-    }
+  },
+  options: {
+    screen: optionsStackNavigator,
+    navigationOptions: {
+      drawerLabel: "Options",
+    },
+  },
 });
 
 export default DrawerNavigator;
--- screens/learning/EditVocableScreen.js
+++ screens/learning/EditVocableScreen.js
@@ -1,17 +1,100 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, ScrollView, Text, Button, ActivityIndicator, Alert } from "react-native";
+import { Formik } from "formik";
+import Input from "../../components/Input";
+import Label from "../../components/Label";
+import { useDispatch } from "react-redux";
+import * as vocableActions from "../../store/actions/vocables";
+import Colors from "../../constants/Colors";
+import * as yup from "yup";
+import DefaultValues from "../../constants/DefaultValues";
 
-const EditVocableScreen = props => {
-    return (
-        <View>
-
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    
+const yupSchema = yup.object({
+  wordENG: yup.string().required(),
+  wordDE: yup.string().required(),
 });
 
+const EditVocableScreen = (props) => {
+  // States
+  const [hasError, setHasError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (hasError) {
+      Alert.alert("An error occured!", hasError.message, [{ text: "Okay" }]);
+    }
+  }, [hasError]);
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Formik
+          initialValues={{
+            wordENG: props.navigation.getParam("vocable").wordENG ? props.navigation.getParam("vocable").wordENG : "",
+            wordDE: props.navigation.getParam("vocable").wordDE ? props.navigation.getParam("vocable").wordDE : "",
+          }}
+          validationSchema={yupSchema}
+          onSubmit={async (values, actions) => {
+            // Submit button pressed
+            setIsLoading(true);
+            try {
+              await dispatch(vocableActions.updateVocable(props.navigation.getParam("vocable").id, values.wordENG, values.wordDE));
+              actions.resetForm();
+              props.navigation.goBack();
+            } catch (error) {
+              setHasError(error);
+            }
+            setIsLoading(false);
+          }}>
+          {(formikProps) => (
+            <View>
+              <Label title="English:" />
+              <Input
+                onBlur={formikProps.handleBlur("wordENG")}
+                placeholder="Enter the english Word"
+                onChangeText={formikProps.handleChange("wordENG")}
+                value={formikProps.values.wordENG}
+                editable={isLoading ? false : true}
+              />
+              <Text style={styles.errorText}>{formikProps.touched.wordENG && formikProps.errors.wordENG}</Text>
+
+              <Label title="German:" />
+              <Input
+                onBlur={formikProps.handleBlur("wordDE")}
+                placeholder="Enter the german Word"
+                onChangeText={formikProps.handleChange("wordDE")}
+                value={formikProps.values.wordDE}
+                editable={isLoading ? false : true}
+              />
+              <Text style={styles.errorText}>{formikProps.touched.wordDE && formikProps.errors.wordDE}</Text>
+
+              {isLoading ? <ActivityIndicator size="small" color={Colors.grey} /> : <Button title="Change" onPress={formikProps.handleSubmit} />}
+            </View>
+          )}
+        </Formik>
+      </View>
+    </ScrollView>
+  );
+};
+
+EditVocableScreen.navigationOptions = (navigationData) => {
+  return {
+    title: "Edit Vocable",
+  };
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 15,
+  },
+  errorText: {
+    color: "#ee0000",
+    marginTop: 5,
+    marginBottom: 10,
+    fontFamily: DefaultValues.fontBold,
+  },
+});
 
 export default EditVocableScreen;
