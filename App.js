956e185d5bf3fc0e43c374a5ba5b2863aa1a3a32
Maurice Bertram <Stricker78@live.de>
2022-05-28 00:50:09 +0200

    Updated Expo and React Native

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { enableScreens } from "react-native-screens";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 import AppNavigator from "./navigation/AppNavigator";
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import vocableReducer from "./store/reducers/vocables";
@@ -36,8 +37,36 @@ export default function App() {
     //enableScreens();
 
     // States
-    const [fontLoaded, setFontLoaded] = useState(false);
+    const [appIsReady, setAppIsReady] = useState(false);
 
+    useEffect(() => {
+        async function prepareApp() {
+            try {
+                // Keep the splash screen visible while we fetch resources
+                await SplashScreen.preventAutoHideAsync();
+                await initLanguage();
+                // Start Bugsnag to log crashes or errors while running this app
+                Bugsnag.start();
+                // Pre-load fonts
+                await Font.loadAsync({
+                    roboto: require("./assets/fonts/roboto-regular.ttf"),
+                    "roboto-bold": require("./assets/fonts/roboto-bold.ttf"),
+                });
+                // Artificially delay for two seconds to simulate a slow loading
+                // experience. Please remove this if you copy and paste the code!
+                //await new Promise((resolve) => setTimeout(resolve, 2000));
+            } catch (e) {
+                console.log(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+            }
+        }
+
+        prepareApp();
+    }, []);
+
+    /*
     // Function to load Fonts
     const fetchFonts = async () => {
         await initLanguage();
@@ -47,24 +76,30 @@ export default function App() {
             "roboto-bold": require("./assets/fonts/roboto-bold.ttf"),
         });
     };
+    */
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            // This tells the splash screen to hide immediately! If we call this after
+            // `setAppIsReady`, then we may see a blank screen while the app is
+            // loading its initial state and rendering its first pixels. So instead,
+            // we hide the splash screen once we know the root view has already
+            // performed layout.
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
 
     // Expo loading screen (Check if font loaded)
-    if (!fontLoaded) {
-        return (
-            <AppLoading
-                startAsync={fetchFonts}
-                onFinish={() => {
-                    setFontLoaded(true);
-                }}
-                onError={console.error}
-            />
-        );
+    if (!appIsReady) {
+        return null;
     }
 
     return (
-        <Provider store={store}>
-            <AppNavigator />
-        </Provider>
+        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+            <Provider store={store}>
+                <AppNavigator />
+            </Provider>
+        </View>
     );
 }
 
--- android/app/src/main/java/de/dreamgamer/vocabletasks/MainApplication.java
+++ android/app/src/main/java/de/dreamgamer/vocabletasks/MainApplication.java
@@ -2,45 +2,36 @@ package de.dreamgamer.vocabletasks;
 
 import android.app.Application;
 import android.content.Context;
-import android.net.Uri;
+import android.content.res.Configuration;
+import androidx.annotation.NonNull;
 
 import com.bugsnag.android.BreadcrumbType;
 import com.bugsnag.android.Bugsnag;
-import com.bugsnag.android.Configuration;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.zoontek.rnlocalize.RNLocalizePackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.zoontek.rnlocalize.RNLocalizePackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import de.dreamgamer.vocabletasks.generated.BasePackageList;
+import de.dreamgamer.vocabletasks.newarchitecture.MainApplicationReactNativeHost;
 
-import org.unimodules.adapters.react.ReactAdapterPackage;
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.Package;
-import org.unimodules.core.interfaces.SingletonModule;
-import expo.modules.constants.ConstantsPackage;
-import expo.modules.permissions.PermissionsPackage;
-import expo.modules.filesystem.FileSystemPackage;
-import expo.modules.updates.UpdatesController;
-
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import expo.modules.ApplicationLifecycleDispatcher;
+import expo.modules.ReactNativeHostWrapper;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import javax.annotation.Nullable;
 
 public class MainApplication extends Application implements ReactApplication {
-  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(
-    new BasePackageList().getPackageList()
-  );
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
+    this,
+    new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -48,8 +39,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
       return packages;
     }
 
@@ -57,41 +50,24 @@ public class MainApplication extends Application implements ReactApplication {
     protected String getJSMainModuleName() {
       return "index";
     }
+  });
 
-    @Override
-    protected JSIModulePackage getJSIModulePackage() {
-      return new ReanimatedJSIModulePackage();
-    }
-
-    @Override
-    protected @Nullable String getJSBundleFile() {
-      if (BuildConfig.DEBUG) {
-        return super.getJSBundleFile();
-      } else {
-        return UpdatesController.getInstance().getLaunchAssetFile();
-      }
-    }
-
-    @Override
-    protected @Nullable String getBundleAssetName() {
-      if (BuildConfig.DEBUG) {
-        return super.getBundleAssetName();
-      } else {
-        return UpdatesController.getInstance().getBundleAssetName();
-      }
-    }
-  };
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new ReactNativeHostWrapper(this, new MainApplicationReactNativeHost(this));
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-
-    Configuration config = Configuration.load(this);
+    com.bugsnag.android.Configuration config = com.bugsnag.android.Configuration.load(this);
     config.setEnabledBreadcrumbTypes(new HashSet<BreadcrumbType>() {{
       add(BreadcrumbType.NAVIGATION);
       add(BreadcrumbType.REQUEST);
@@ -104,13 +80,18 @@ public class MainApplication extends Application implements ReactApplication {
     }});
     Bugsnag.start(this, config);
 
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
 
-    if (!BuildConfig.DEBUG) {
-      UpdatesController.initialize(this);
-    }
-
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+  }
+
+  @Override
+  public void onConfigurationChanged(@NonNull Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
 
   /**
