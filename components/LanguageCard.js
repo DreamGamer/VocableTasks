e4df2c5f76d0ec664b6f2afdae0d3ede6185c96a
Maurice Bertram <Stricker78@live.de>
2022-05-28 16:48:05 +0200

    Add Change Language Screen And Option

---
+++ components/LanguageCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Colors from "../constants/Colors";
+import Flag from "react-native-flags";
+import { Ionicons } from "@expo/vector-icons";
+import DefaultValues from "../constants/DefaultValues";
+
+const LanguageCard = props => {
+    return (
+        <TouchableOpacity onPress={props.onPress}>
+            <View style={{ ...styles.container, ...(props.choosedLanguage ? styles.choosedContainer : null) }}>
+                <View style={styles.box}>
+                    <View style={styles.leftSide}>
+                        <Flag code={props.code} size={32} style={styles.flag} />
+                        <Text style={{ ...styles.title, ...(props.choosedLanguage ? styles.choosedTitle : null) }}>{props.title}</Text>
+                    </View>
+                    <View style={styles.rightSide}></View>
+                    {props.choosedLanguage ? <Ionicons name="checkmark" size={23} color={Colors.white} /> : null}
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.white,
+        height: 60,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+    },
+    choosedContainer: {
+        backgroundColor: Colors.info,
+    },
+    box: {
+        paddingHorizontal: 5,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    leftSide: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    rightSide: {},
+    flag: {
+        marginRight: 10,
+    },
+    title: {
+        color: Colors.black,
+        fontFamily: DefaultValues.fontRegular,
+        fontSize: 16,
+    },
+    choosedTitle: {
+        color: Colors.white,
+    },
+});
+
+export default LanguageCard;
--- screens/settings/ChangeLanguageScreen.js
+++ screens/settings/ChangeLanguageScreen.js
@@ -1,23 +1,66 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import I18n from "../../i18n/translation";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View, FlatList } from "react-native";
+import { Trans, useTranslation } from "react-i18next";
+import LanguageCard from "../../components/LanguageCard";
+import Spinner from "react-native-loading-spinner-overlay";
+import Bugsnag from "@bugsnag/react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { Translation } from "../../i18n/translation";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const TAG = "[ChangeLanguageScreen]: "; // Console Log Tag
+const APP_LANGUAGE = "app_language"; // AsyncStorage key
 
 const ChangeLanguageScreen = props => {
+    const { t, i18n } = useTranslation();
+    const selectedLanguage = i18n.language;
+    const [isLoading, setIsLoading] = useState(false);
+
+    const availableLanguages = [
+        { code: "england", text: t("englishLanguage"), originalCode: "en" },
+        { code: "DE", text: t("germanLanguage"), originalCode: "de" },
+    ];
+
     return (
         <View>
-            <Text>ChangeLanguageScreen</Text>
+            <Spinner visible={isLoading} />
+
+            <FlatList
+                data={availableLanguages}
+                keyExtractor={item => item.code}
+                renderItem={item => (
+                    <LanguageCard
+                        code={item.item.code}
+                        title={item.item.text}
+                        onPress={async () => {
+                            const language = item.item.originalCode;
+                            try {
+                                // Set current app language to language
+                                await i18n.changeLanguage(language);
+
+                                // Save LanguageCode in Storage
+                                await AsyncStorage.setItem(APP_LANGUAGE, language);
+
+                                // Log sucessfully
+                                console.info(TAG + "Sucessfully changed app language to '" + language + "'");
+                            } catch (error) {
+                                console.warn(TAG + "Catched error while changing language: " + error);
+                                Bugsnag.notify(error);
+                            }
+                        }}
+                        choosedLanguage={item.item.originalCode == selectedLanguage}
+                    />
+                )}></FlatList>
         </View>
-    )
+    );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export const ChangeLanguageScreenOptions = navigationData => {
     return {
-        title: I18n.t("changeLanguage"),
-    }
-}
+        title: <Translation name="changeLanguage" />,
+    };
+};
 
 export default ChangeLanguageScreen;
