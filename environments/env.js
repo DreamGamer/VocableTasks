9f29b1e802d8823009adb829c1266301cac36fb5
Maurice Bertram <stricker78@live.de>
2022-10-04 18:44:37 +0200

    add default environments

---
+++ environments/env.js
@@ -0,0 +1,3 @@
+export const FIREBASE_API_KEY = "[ENTER FIREBASE_API_KEY]";
+export const algoliaSearchAppID = "[ENTER algoliaSearchAppID]";
+export const algoliaSearchApiKey = "[ENTER algoliaSearchApiKey]";
